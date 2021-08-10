231a92332000012cfeb3c1a35f9ca830ef1aec03
SeungSooNAM <68661351+SeungSooNAM@users.noreply.github.com>
2021-08-10 18:03:53 +0900

    남승수-3

--- src/com/itsme/letitgo/login/model/mapper/MemberMapper.java
+++ src/com/itsme/letitgo/login/model/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package com.itsme.letitgo.login.model.mapper;
 
 import com.itsme.letitgo.login.model.dto.MemberDTO;
 
+
 public interface MemberMapper {
 
 	String selectPwd(MemberDTO requestMember);
