ef5fcd688583b004a9b31df61aa0b8c47e27757f
SeungSooNAM <68661351+SeungSooNAM@users.noreply.github.com>
2021-08-15 00:38:12 +0900

    남승수

--- src/com/itsme/letitgo/admin/info/controller/RequestRegistAcceptServlet.java
+++ src/com/itsme/letitgo/admin/info/controller/RequestRegistAcceptServlet.java
@@ -1,11 +1,15 @@
 package com.itsme.letitgo.admin.info.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.itsme.letitgo.admin.info.model.service.RequestJoinService;
 
 
 @WebServlet("/info/accept")
@@ -13,17 +17,35 @@ public class RequestRegistAcceptServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		int coReqNo = Integer.parseInt(request.getParameter("coReqNo"));
+		int coMemNo = Integer.parseInt(request.getParameter("coMemNo"));
 		
-
+		RequestJoinService rJService = new RequestJoinService();
+		
+		int result1 = rJService.JoinRequest1(coReqNo);
+		int result2 = rJService.JoinRequest2(coMemNo);
+		
+		StringBuilder redirectText = new StringBuilder();
+		
+		if(result1 > 0 && result2 > 0) {
+			redirectText.append("<script>alert('채용공고등록을 승인하셨습니다.'); location.href='../../post/insertRequest/select';</script>");
+		} else {
+			redirectText.append("<script>alert('채용공고등록승인에 실패하셨습니다.'); location.href='../../post/insertRequest/select';</script>");
+		}
+		 
+									
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		out.print(redirectText.toString());
+		out.flush();
+		out.close();
 	}
 
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		request.setCharacterEncoding("UTF-8");
-		
-	}
+
 
 }
