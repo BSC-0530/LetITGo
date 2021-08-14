3db030672aa16f2efa96f02cc44da17629a87de8
82106 <82106@DESKTOP-UG1UE1J>
2021-08-15 00:37:52 +0900

    지원하기 , 지원 취소 시작

--- src/com/itsme/letitgo/personal/recruit/jobposting/controller/SelectDetailedJobPostingServlet.java
+++ src/com/itsme/letitgo/personal/recruit/jobposting/controller/SelectDetailedJobPostingServlet.java
@@ -1,7 +1,6 @@
 package com.itsme.letitgo.personal.recruit.jobposting.controller;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -10,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.itsme.letitgo.personal.recruit.jobposting.model.dto.SelectJobPostingDTO;
+import com.itsme.letitgo.personal.recruit.jobposting.model.dto.SelectApplyngYnDTO;
 import com.itsme.letitgo.personal.recruit.jobposting.model.service.SelectJobPostingService;
 
 @WebServlet("/detail/jobPosting/select")
@@ -18,12 +17,21 @@ public class SelectDetailedJobPostingServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		int selectJobPostNo = Integer.parseInt(request.getParameter("selectJobPostNo"));
+		// if문을 통해서 회원번호, 공고번호를 가져온다. 회원번호와 공고 번호를 넘겨줘서 이력서의 지원 여부를 확인한다. 지원여부를 가져온다. 이 때 지원여부가 
+		SelectApplyngYnDTO dto = new SelectApplyngYnDTO();
 		
-		SelectJobPostingService selectJobPostingService = new SelectJobPostingService();
+		int jobPostNo = Integer.parseInt(request.getParameter("selectJobPostNo"));
 		
-		Map<String, Object> jp = selectJobPostingService.selectDetailedJobPosting(selectJobPostNo);
+		dto.setMemNo(3);
+		dto.setJobPostNo(jobPostNo);
 		
+		SelectJobPostingService service = new SelectJobPostingService();
+		
+		// 지원여부 가져온다.
+		boolean applyingResult = service.selectApplyingHistory(dto);
+		
+		// 채용공고 정보 조회
+		Map<String, Object> jp = service.selectDetailedJobPosting(jobPostNo);
 		
 		System.out.println(jp.get("detailedJobPosting"));
 		
