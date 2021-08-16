a1fef6f8351a5acf164ae0f186d0c84a6f1c9c33
SeungSooNAM <68661351+SeungSooNAM@users.noreply.github.com>
2021-08-16 18:47:37 +0900

    남승수

--- src/com/itsme/letitgo/company/info/controller/ResetPassword.java
+++ src/com/itsme/letitgo/company/info/controller/ResetPassword.java
@@ -7,11 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import com.itsme.letitgo.company.info.model.dto.CompanyInfoDTO;
+import com.itsme.letitgo.company.info.model.dto.ModifyChageDTO;
 import com.itsme.letitgo.company.info.model.service.CompanyModifyService;
 
 
-@WebServlet("company/resetpassword")
+@WebServlet("/company/resetpassword")
 public class ResetPassword extends HttpServlet {
 	
        
@@ -25,9 +28,36 @@ public class ResetPassword extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		CompanyInfoDTO companyInfoDTO = new CompanyInfoDTO();
 		
-		companyInfoDTO.getMemPwd();
+		String userId = companyInfoDTO.getMemId();
 		
+		String userNowPwd = request.getParameter("memPwd");
+		String userNewPwd = request.getParameter("chagePwd");
+		String userNewPwdCheck = request.getParameter("userNewPwdCheck");
 		
+		ModifyChageDTO modifyChangeDTO = new ModifyChageDTO();
+		
+		modifyChangeDTO.setMemberId(userId);
+		modifyChangeDTO.setNowPwd(userNowPwd);
+		modifyChangeDTO.setNewPwd(userNewPwd);
+		
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder(); 
+		
+		 if (passwordEncoder.matches(userNewPwdCheck, userNewPwd)) {
+				 
+				 CompanyModifyService companyModifyService = new CompanyModifyService();
+		         int MemberPWDUpdate = companyModifyService.PwdCheck(modifyChangeDTO);
+		         
+		         if(MemberPWDUpdate > 0) {
+		            response.sendRedirect(request.getContextPath());   //경로
+		            System.out.println(request.getContextPath());  //"groubear" 주소이다
+		            
+		         } else {
+		            request.setAttribute("message", "비밀번호 변경실패");
+		            request.getRequestDispatcher("/WEB-INF/views/common/failed.jsp").forward(request, response);
+		         }
+		         
+		      } else {
+		         System.out.println("비밀번호가 다릅니다.");
+		      }
 	}
-
 }
---
+++ src/com/itsme/letitgo/company/info/model/mapper/ModifyPassworMapper.java
@@ -0,0 +1,9 @@
+package com.itsme.letitgo.company.info.model.mapper;
+
+import com.itsme.letitgo.company.info.model.dto.ModifyChageDTO;
+
+public interface ModifyPassworMapper {
+
+	int ModifyPassword(ModifyChageDTO modifyChangeDTO);
+
+}
