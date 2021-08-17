462406912d875b4b47d08124a29bd6ca6bd84543
Taehun Kim <tehonkim.kim@gamil.com>
2021-08-18 02:20:40 +0900

    상세조회 완료 & 수정하기 해야함 & 개인이 개인정보처리방침, 이용약관 보는 기능 해야함

--- src/com/itsme/letitgo/admin/board/notice/controller/PersonalInfoPolicyDetailViewServlet.java
+++ src/com/itsme/letitgo/admin/board/notice/controller/PersonalInfoPolicyDetailViewServlet.java
@@ -1,3 +1,60 @@
+//package com.itsme.letitgo.admin.board.notice.controller;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import com.itsme.letitgo.admin.board.notice.model.dto.PersonalInfoPolicyDTO;
+//import com.itsme.letitgo.admin.board.notice.model.service.PersonalInfoPolicyService;
+//
+//@WebServlet("/personalinfo/policy/detailview")
+//public class PersonalInfoPolicyDetailViewServlet extends HttpServlet {
+//
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+////      본근님꺼
+//		PersonalInfoPolicyService personalInfoPolicyService = new PersonalInfoPolicyService();
+//		
+//		int postNo = Integer.parseInt(request.getParameter("postNo"));
+//		
+//		PersonalInfoPolicyDTO detailView = personalInfoPolicyService.selectDetailList(postNo);
+//		
+//		System.out.println(detailView);
+//		
+//		request.setAttribute("detailView", detailView);
+//		
+//		String path="";
+//		
+//		path = "/WEB-INF/views/admin/adminPolicyAndGuideLineDetailView.jsp";
+//		request.getRequestDispatcher(path).forward(request, response);
+//		
+////		< 내가 만든거 안넘어가가가가가가가가가 >
+////		int postNo = Integer.parseInt(request.getParameter("postNo")); //selectedDetailPostNo : 뷰 페이지에 있는 변수명
+////		System.out.println("request selectedDetailPostNo : " + postNo);
+////		
+////		List<PersonalInfoPolicyDTO> detailView = new PersonalInfoPolicyService().selectDetailList(postNo);
+////		
+////		System.out.println("서블렛 : " + detailView);
+////		
+////		String path = "";
+////		
+////		if(detailView != null) {
+////			path = "/WEB-INF/views/admin/adminPolicyAndGuideLineDetailView.jsp";
+////			request.setAttribute("detailList", detailView);	//detailList 어디서 받아오는지
+////		}
+////		
+////		request.getRequestDispatcher(path).forward(request, response);
+//		
+//		
+//	}
+//
+//}
+
 package com.itsme.letitgo.admin.board.notice.controller;
 
 import java.io.IOException;
@@ -20,7 +77,8 @@ public class PersonalInfoPolicyDetailViewServlet extends HttpServlet {
 		int postNo = Integer.parseInt(request.getParameter("postNo")); //selectedDetailPostNo : 뷰 페이지에 있는 변수명
 		System.out.println("request selectedDetailPostNo : " + postNo);
 		
-		List<PersonalInfoPolicyDTO> detailView = new PersonalInfoPolicyService().selectDetailList(postNo);
+//		List<PersonalInfoPolicyDTO> detailView = new PersonalInfoPolicyService().selectDetailList(postNo);
+		PersonalInfoPolicyDTO detailView = new PersonalInfoPolicyService().selectDetailList(postNo);
 		
 		System.out.println("서블렛 : " + detailView);
 		
@@ -38,3 +96,5 @@ public class PersonalInfoPolicyDetailViewServlet extends HttpServlet {
 	}
 
 }
+
+
--- src/com/itsme/letitgo/login/controller/idCheckServlet.java
+++ src/com/itsme/letitgo/login/controller/idCheckServlet.java
@@ -1,37 +1,37 @@
-package com.itsme.letitgo.login.controller;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import com.itsme.letitgo.personal.regist.model.service.MemberService;
-
-/**
- * Servlet implementation class idCheckServlet
- */
-@WebServlet("/member/idCheck")
-public class idCheckServlet extends HttpServlet {
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String id = request.getParameter("memberId");
-		
-		MemberService ms = new MemberService();
-		
-		int result = ms.idCheck(id);
-		
-		response.getWriter().print((result > 0) ? "no" : "ok");
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
-}
+//package com.itsme.letitgo.login.controller;
+//
+//import java.io.IOException;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import com.itsme.letitgo.personal.regist.model.service.MemberService;
+//
+///**
+// * Servlet implementation class idCheckServlet
+// */
+//@WebServlet("/member/idCheck")
+//public class idCheckServlet extends HttpServlet {
+//	
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		
+//		String id = request.getParameter("memberId");
+//		
+//		MemberService ms = new MemberService();
+//		
+//		int result = ms.idCheck(id);
+//		
+//		response.getWriter().print((result > 0) ? "no" : "ok");
+//	}
+//
+//	/**
+//	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+//	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+//
+//}
