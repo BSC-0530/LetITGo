d098cb2571181dc56936f423073b07548519d0d1
SeungSooNAM <68661351+SeungSooNAM@users.noreply.github.com>
2021-08-18 10:04:03 +0900

    남승수

--- src/com/itsme/letitgo/admin/payment/controller/ProductModifyServlet.java
+++ src/com/itsme/letitgo/admin/payment/controller/ProductModifyServlet.java
@@ -7,19 +7,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.itsme.letitgo.admin.payment.model.dto.ProductListDTO;
+import com.itsme.letitgo.admin.payment.model.service.ProductModifyService;
+
 
 @WebServlet("/admin/product/update")
 public class ProductModifyServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		
+		String path = "/WEB-INF/views/admin/adminProductModify.jsp";
+		
+		request.getRequestDispatcher(path).forward(request, response);
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		request.setCharacterEncoding("UTF-8");
+		
+		String productName = request.getParameter("productName");
+		int productPrice = Integer.parseInt(request.getParameter("productPrice"));
+		String productKinds = request.getParameter("productKinds");
+		String productStatus = request.getParameter("productStatus");
+		
+		ProductListDTO productListDTO = new ProductListDTO();
+		productListDTO.setProductName(productName);
+		productListDTO.setProductPrice(productPrice);
+		productListDTO.setProductKinds(productKinds);
+		productListDTO.setProductStatus(productStatus);
+		
+		String path = "";
+		
+		int result = new ProductModifyService().productModify(productListDTO);
+		
+		if(result > 0){
+			path = "/WEB-INF/views/admin/adminProductModify.jsp";
+		}else {
+			
+		}
 	}
-	
-	
 
 }
