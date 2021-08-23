6090572019d1951936b13bf7f7ab39bcf62fc525
SeungSooNAM <68661351+SeungSooNAM@users.noreply.github.com>
2021-08-23 14:09:34 +0900

    남승수

--- src/com/itsme/letitgo/company/info/model/service/CompanyModifyService.java
+++ src/com/itsme/letitgo/company/info/model/service/CompanyModifyService.java
@@ -16,15 +16,20 @@ public class CompanyModifyService {
 		
 		SqlSession session = getSqlSession();	
 		
+		/*AdminPaymentMapper에 session을 넣는다*/
 		ModifyPassworMapper mapper = session.getMapper(ModifyPassworMapper.class);
 		
+		/*mapper에 productList(companyInfoDTO)를 담고 메소드 생성*/
 		int result = mapper.resetPassword(companyInfoDTO);
 		
 		if(result > 0) {
+			/*result가 1이상이면 커밋을 한다. 즉!! result가 성공하면 커밋을 한다.*/
 			session.commit();
 		}else {
+			/*result가 실패를 하면 롤백을 한다.*/
 			session.rollback();
 		}
+		/*session을 닫는다(안 닫으면 DB에서 문제가 발생한다.)*/
 		session.close();
 		
 		return result;
